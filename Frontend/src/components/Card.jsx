963c57db4282ffd6c8a47edf7cf52a983d407c47
Apurba Sau <156792303+ApurbaSau1@users.noreply.github.com>
2025-10-13 20:52:35 +0530

    Update Card.jsx

--- Frontend/src/components/Card.jsx
+++ Frontend/src/components/Card.jsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import { userDataContext } from '../context/usercontext'
+import { userDataContext } from '../context/UserContext'
 
 const Card = ({image}) => {
    const{serverUrl,
